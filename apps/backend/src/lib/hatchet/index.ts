785de3bdc66709ed6e7f07a604edea0485610fd1
coolpengwing <coolpengwing@gmail.com>
2025-08-31 18:52:22 +0100

    fix: fix missing hatchet init configuration to connect to engine properly

--- apps/backend/src/lib/hatchet/index.ts
+++ apps/backend/src/lib/hatchet/index.ts
@@ -4,7 +4,9 @@ import { logger } from "../../utils/logger/index.js";
 let hatchet: Hatchet;
 
 export const initHatchetClient = (): void => {
-  hatchet = Hatchet.init();
+  hatchet = Hatchet.init({
+    host_port: process.env.HATCHET_CLIENT_HOST_PORT || "http://localhost:7077",
+  });
   logger.info("Hatchet client is initialised");
 };
 
